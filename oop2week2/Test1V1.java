15342540bc1b1e65cf92212f7ff324c4b5c8db06
raghadarabiyyat <148454862+raghadarabiyyat@users.noreply.github.com>
2023-10-27 21:51:04 +0300

    Add files via upload

---
+++ oop2week2/Test1V1.java
@@ -0,0 +1,12 @@
+package oop2week2;
+    import javax.swing.JOptionPane;
+public class Test1V1 {
+    public static void main(String[] args) {
+        String str1= JOptionPane.showInputDialog("Enter n1");
+        String str2= JOptionPane.showInputDialog("Enter n2");
+        int n1=Integer.parseInt(str1);
+        int n2=Integer.parseInt(str2);
+        
+    }
+    
+}
---
+++ oop2week2/Util.java
@@ -0,0 +1,13 @@
+package oop2week2;
+
+public class Util {
+
+    public static void print(String val) {
+        System.out.print(val);
+    }
+
+    public static void print(int val) {
+        System.out.print(val);
+
+    }
+}
