6d339cfb2cd431cf4c20694b36dd9ff2d5202b67
raghadarabiyyat <148454862+raghadarabiyyat@users.noreply.github.com>
2023-12-19 19:17:50 +0300

    Update Facade.java

--- Facade/Facade.java
+++ Facade/Facade.java
@@ -2,8 +2,8 @@ package Facade;
 
 public class Facade {
 
-    public SubSystem1 subsystem1 = new SubSystem1();
-    public SubSystem2 subsystem2 = new SubSystem2();
+    private SubSystem1 subsystem1 = new SubSystem1();
+    private SubSystem2 subsystem2 = new SubSystem2();
 
     public void operate(){
         System.out.println("Facade is coordenating the subsystem ");
